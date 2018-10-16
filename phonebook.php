f30780292761e939ac1e49063309bd7ef23d11a4
Elena741 <solo741@yandex.ru>
2018-10-16 22:08:40 +0400

    Телефонная книга в виде html таблицы.

---
+++ phonebook.php
@@ -0,0 +1,38 @@
+﻿<?php
+$a = file_get_contents('spisok3.json');
+$json_de = json_decode($a, true);
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Телефонная книга</title>
+    <style>
+        body {
+            padding: 0 20px;
+        }
+        td {
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+
+  <table border="1">
+   <caption><h2>Телефонная книга</h2></caption>
+    <tr>
+        <th>Имя</th>
+        <th>Фамилия</th>
+        <th>Адрес</th>
+        <th>Телефоны</th>
+   </tr>
+<?php
+foreach ($json_de as $key => $arr) {
+?>
+
+<tr><td><?= $arr['firstName'] ?></td><td><?= $arr['lastName'] ?></td><td><?= $arr['address'] ?></td><td><?= $arr['phoneNumber'] ?></td></tr>
+
+<?php
+}
+?>
+</body>
+</html>
